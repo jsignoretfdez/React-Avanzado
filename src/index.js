7506da91bcd3a70b1ac9fd7752b1b24d8e621a67
David Jiménez <davidjj76@gmail.com>
2020-11-18 00:19:15 +0100

    Error boundary

--- src/index.js
+++ src/index.js
@@ -12,9 +12,31 @@ const auth = storage.get('auth') || { id: null, accessToken: null };
 
 configureClient(auth.accessToken);
 
+class ErrorBoundary extends React.Component {
+  state = {
+    hasError: false,
+  };
+
+  componentDidCatch(error, errorInfo) {
+    console.log(error, errorInfo);
+    this.setState({ hasError: true });
+  }
+
+  render() {
+    const { hasError } = this.state;
+    if (hasError) {
+      return <div>Something went wrong!</div>;
+    }
+
+    return this.props.children;
+  }
+}
+
 ReactDOM.render(
-  <BrowserRouter>
-    <App initiallyLooggedUserId={auth.id} />
-  </BrowserRouter>,
+  <ErrorBoundary>
+    <BrowserRouter>
+      <App initiallyLooggedUserId={auth.id} />
+    </BrowserRouter>
+  </ErrorBoundary>,
   document.getElementById('root')
 );

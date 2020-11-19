bfdb6c98f54502aa37ad395169686c91a0ad7b3f
David Jiménez <davidjj76@gmail.com>
2020-11-19 22:27:30 +0100

    Use useState

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import T from 'prop-types';
 import { Redirect, Route, Switch } from 'react-router-dom';
 
@@ -9,67 +9,56 @@ import NewTweetPage from '../tweets/NewTweetPage';
 import PrivateRoute from '../auth/PrivateRoute';
 import { AuthContextProvider } from '../auth/context';
 
-class App extends React.Component {
-  tweetsPageRef = React.createRef();
-  loginPageRef = React.createRef();
-  state = {
-    loggedUserId: this.props.initiallyLooggedUserId,
-  };
+function App({ initiallyLooggedUserId }) {
+  const [loggedUserId, setLoggedUserId] = useState(initiallyLooggedUserId);
 
-  handleLogin = (loggedUserId, cb) => this.setState({ loggedUserId }, cb);
+  const handleLogin = loggedUserId =>
+    new Promise(resolve => {
+      setLoggedUserId(loggedUserId);
+      resolve();
+    });
+  const handleLogout = () => setLoggedUserId(null);
 
-  handleLogout = () => this.setState({ loggedUserId: null });
-
-  componentDidMount() {
-    // console.log(this.tweetsPageRef);
-    // if (this.tweetsPageRef.current) {
-    //   this.tweetsPageRef.current.getTweets();
-    // }
-  }
-
-  render() {
-    const { loggedUserId } = this.state;
-    return (
-      <AuthContextProvider
-        value={{
-          isLogged: !!loggedUserId,
-          onLogin: this.handleLogin,
-          onLogout: this.handleLogout,
-        }}
-      >
-        <div className="App">
-          <Switch>
-            <Route path="/" exact>
-              {({ history }) => <TweetsPage history={history} />}
-            </Route>
-            <PrivateRoute path="/tweet" exact>
-              <NewTweetPage />
-            </PrivateRoute>
-            <Route path="/tweet/:tweetId" exact component={TweetPage} />
-            <Route path="/login" exact>
-              {({ history }) => (
-                <LoginPage onLogin={this.handleLogin} history={history} />
-              )}
-            </Route>
-            <Route path="/404" exact>
-              <div
-                style={{
-                  textAlign: 'center',
-                  fontSize: 48,
-                  fontWeight: 'bold',
-                }}
-              >
-                404 | Not found page
-              </div>
-            </Route>
-            <Route>
-              <Redirect to="/404" />
-            </Route>
-          </Switch>
-        </div>
-      </AuthContextProvider>
-    );
-  }
+  return (
+    <AuthContextProvider
+      value={{
+        isLogged: !!loggedUserId,
+        onLogin: handleLogin,
+        onLogout: handleLogout,
+      }}
+    >
+      <div className="App">
+        <Switch>
+          <Route path="/" exact>
+            {({ history }) => <TweetsPage history={history} />}
+          </Route>
+          <PrivateRoute path="/tweet" exact>
+            <NewTweetPage />
+          </PrivateRoute>
+          <Route path="/tweet/:tweetId" exact component={TweetPage} />
+          <Route path="/login" exact>
+            {({ history }) => (
+              <LoginPage onLogin={handleLogin} history={history} />
+            )}
+          </Route>
+          <Route path="/404" exact>
+            <div
+              style={{
+                textAlign: 'center',
+                fontSize: 48,
+                fontWeight: 'bold',
+              }}
+            >
+              404 | Not found page
+            </div>
+          </Route>
+          <Route>
+            <Redirect to="/404" />
+          </Route>
+        </Switch>
+      </div>
+    </AuthContextProvider>
+  );
 }
 
 App.propTypes = {
--- src/components/auth/LoginPage.js
+++ src/components/auth/LoginPage.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState } from 'react';
 import T from 'prop-types';
 
 import Button from '../shared/Button';
@@ -8,128 +7,71 @@ import { login } from '../../api/auth';
 
 import './LoginPage.css';
 
-function copyStyles(sourceDoc, targetDoc) {
-  Array.from(sourceDoc.styleSheets).forEach(styleSheet => {
-    if (styleSheet.cssRules) {
-      // for <style> elements
-      const newStyleEl = sourceDoc.createElement('style');
+function LoginPage({ onLogin, history }) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [submitting, setSubmitting] = useState(false);
+  const [error, setError] = useState(null);
 
-      Array.from(styleSheet.cssRules).forEach(cssRule => {
-        // write the text of each rule into the body of the style element
-        newStyleEl.appendChild(sourceDoc.createTextNode(cssRule.cssText));
-      });
+  const handleEmailChange = event => setEmail(event.target.value);
+  const handlePasswordChange = event => setPassword(event.target.value);
 
-      targetDoc.head.appendChild(newStyleEl);
-    } else if (styleSheet.href) {
-      // for <link> elements loading CSS from a URL
-      const newLinkEl = sourceDoc.createElement('link');
-
-      newLinkEl.rel = 'stylesheet';
-      newLinkEl.href = styleSheet.href;
-      targetDoc.head.appendChild(newLinkEl);
-    }
-  });
-}
-class LoginPage extends React.Component {
-  state = {
-    form: {
-      email: '',
-      password: '',
-    },
-    submitting: false,
-    error: null,
-  };
-
-  containerEl = document.createElement('div');
-  externalWindow = null;
-
-  handleChange = event => {
-    this.setState(state => ({
-      form: { ...state.form, [event.target.name]: event.target.value },
-    }));
-  };
-
-  handleSubmit = async event => {
-    const { onLogin, history } = this.props;
-    const { form: crendentials } = this.state;
+  const handleSubmit = async event => {
+    const crendentials = { email, password };
     event.preventDefault();
-    this.setState({ submitting: true });
+    setSubmitting(true);
     try {
       const loggedUserId = await login(crendentials);
-      this.setState({ submitting: false, error: null });
-      onLogin(loggedUserId, () => history.push('/tweet'));
+      setError(null);
+      onLogin(loggedUserId).then(() => history.push('/tweet'));
     } catch (error) {
-      this.setState({ submitting: false, error });
+      setError(error);
+    } finally {
+      setSubmitting(false);
     }
   };
 
-  componentDidMount() {
-    this.externalWindow = window.open(
-      '',
-      '',
-      'width=600,height=400,left=200,top=200'
-    );
-    this.externalWindow.document.body.appendChild(this.containerEl);
-    copyStyles(document, this.externalWindow.document);
-  }
-
-  componentWillUnmount() {
-    console.log('componentWillUnmount');
-    this.externalWindow.close();
-  }
-
-  canSubmit = () => {
-    const {
-      form: { email, password },
-      submitting,
-    } = this.state;
+  const canSubmit = () => {
     return !submitting && email && password;
   };
 
-  render() {
-    const {
-      form: { email, password },
-      error,
-    } = this.state;
-
-    return ReactDOM.createPortal(
-      <div className="loginPage">
-        <h1 className="loginPage-title">Log in to Twitter</h1>
-        <form onSubmit={this.handleSubmit}>
-          <FormField
-            type="text"
-            name="email"
-            label="phone, email or username"
-            className="loginPage-field"
-            value={email}
-            onChange={this.handleChange}
-          />
-          <FormField
-            type="password"
-            name="password"
-            label="password"
-            className="loginPage-field"
-            value={password}
-            onChange={this.handleChange}
-          />
-          <Button
-            type="submit"
-            className="loginPage-submit"
-            variant="primary"
-            disabled={!this.canSubmit()}
-          >
-            Log in
-          </Button>
-        </form>
-        {error && <div className="loginPage-error">{error.message}</div>}
-      </div>,
-      this.containerEl
-    );
-  }
+  return (
+    <div className="loginPage">
+      <h1 className="loginPage-title">Log in to Twitter</h1>
+      <form onSubmit={handleSubmit}>
+        <FormField
+          type="text"
+          name="email"
+          label="phone, email or username"
+          className="loginPage-field"
+          value={email}
+          onChange={handleEmailChange}
+        />
+        <FormField
+          type="password"
+          name="password"
+          label="password"
+          className="loginPage-field"
+          value={password}
+          onChange={handlePasswordChange}
+        />
+        <Button
+          type="submit"
+          className="loginPage-submit"
+          variant="primary"
+          disabled={!canSubmit()}
+        >
+          Log in
+        </Button>
+      </form>
+      {error && <div className="loginPage-error">{error.message}</div>}
+    </div>
+  );
 }
 
 LoginPage.propTypes = {
   onLogin: T.func.isRequired,
+  history: T.shape({ push: T.func.isRequired }).isRequired,
 };
 
 export default LoginPage;
